ae8211b1f8febf2fbf6006c956fe507523e20790
Alberto Procacci <alberto.procacci@gmail.com>
2021-12-25 15:13:44 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+      name='OpenMEASURE',
+      version='0.0.1',
+      description='Python package for soft sensing applications',
+      py_modules=['OpenMEASURE'],
+      package_dir={'':'src'},
+      )
