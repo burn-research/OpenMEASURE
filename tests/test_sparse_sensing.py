c7824c1bd1c088982f0735874dd13091b8cbf431
Alberto Procacci <alberto.procacci@gmail.com>
2022-01-04 12:49:44 +0100

    added error checking in tests

--- tests/test_sparse_sensing.py
+++ tests/test_sparse_sensing.py
@@ -50,9 +50,9 @@ class testSPR(unittest.TestCase):
     def tearDown(self):
         pass
     
-    # def test_init(self):
-    #     with self.assertRaises(TypeError):
-    #         sps.SPR('X', 5)
+    def test_init(self):
+        with self.assertRaises(TypeError):
+            sps.SPR('X', 5)
     
     def test_scale_data(self):
         spr = sps.SPR(self.X, self.n_features)
@@ -71,8 +71,8 @@ class testSPR(unittest.TestCase):
         y0 = spr.scale_vector(self.y, 'standard')
         
         np.testing.assert_array_equal(np.round(y0,2), self.y0)
-        # with self.assertRaises(NotImplementedError):
-        #     spr.scale_vector(self.y, 'test')      
+        with self.assertRaises(NotImplementedError):
+            spr.scale_vector(self.y, 'test')      
 
     def test_unscale_data(self):
         spr = sps.SPR(self.X, self.n_features)
@@ -80,8 +80,8 @@ class testSPR(unittest.TestCase):
         x = spr.unscale_data(self.x0, 'standard')
 
         np.testing.assert_array_equal(np.round(x,2), self.x)
-        # with self.assertRaises(NotImplementedError):
-        #     spr.unscale_data(self.x0, 'test')
+        with self.assertRaises(NotImplementedError):
+            spr.unscale_data(self.x0, 'test')
 
     def test_decomposition(self):
         spr = sps.SPR(self.X, self.n_features)
@@ -89,8 +89,8 @@ class testSPR(unittest.TestCase):
         U, exp_variance = spr.decomposition(X0, 'POD')
     
         np.testing.assert_array_equal(np.round(U,2), self.U)
-        # with self.assertRaises(NotImplementedError):
-        #     U = spr.decomposition(X0, 'test')
+        with self.assertRaises(NotImplementedError):
+            U = spr.decomposition(X0, 'test')
         
     def test_reduction(self):
         spr = sps.SPR(self.X, self.n_features)
@@ -100,13 +100,13 @@ class testSPR(unittest.TestCase):
         
         np.testing.assert_array_equal(Ur, U[:,0, np.newaxis])
         
-        # with self.assertRaises(ValueError):
-        #     Ur = spr.reduction(U, exp_variance, 'variance', 200)
-        #     Ur = spr.reduction(U, exp_variance, 'number', 5)
-        #     Ur = spr.reduction(U, exp_variance, 'test', 1)
+        with self.assertRaises(ValueError):
+            Ur = spr.reduction(U, exp_variance, 'variance', 200)
+            Ur = spr.reduction(U, exp_variance, 'number', 5)
+            Ur = spr.reduction(U, exp_variance, 'test', 1)
         
-        # with self.assertRaises(TypeError):
-        #     Ur = spr.reduction(U, exp_variance, 'number', 3.5)
+        with self.assertRaises(TypeError):
+            Ur = spr.reduction(U, exp_variance, 'number', 3.5)
         
     def test_optimal_placement(self):
         spr = sps.SPR(self.X, self.n_features)
@@ -119,15 +119,15 @@ class testSPR(unittest.TestCase):
         x_rec = spr.fit_predict(self.C, self.y, 'standard', 'number', 2)
         
         np.testing.assert_array_equal(np.round(x_rec,2), self.x_rec)
-        # with self.assertRaises(ValueError):
-        #     x_rec = spr.fit_predict(self.C[0,:], self.y, 'standard', 'number', 2)
-        #     x_rec = spr.fit_predict(self.C[:,:2], self.y, 'standard', 'number', 2)
-        #     x_rec = spr.fit_predict(self.C, self.y[:,0], 'standard', 'number', 2)
+        with self.assertRaises(ValueError):
+            x_rec = spr.fit_predict(self.C[0,:], self.y, 'standard', 'number', 2)
+            x_rec = spr.fit_predict(self.C[:,:2], self.y, 'standard', 'number', 2)
+            x_rec = spr.fit_predict(self.C, self.y[:,0], 'standard', 'number', 2)
 
     def test_predict(self):
         spr = sps.SPR(self.X, self.n_features)
-        # with self.assertRaises(AttributeError):
-        #     spr.predict(self.y)
+        with self.assertRaises(AttributeError):
+            spr.predict(self.y)
             
         spr.fit_predict(self.C, self.y, 'standard', 'number', 2)
         x_rec = spr.predict(self.y)
